93c8054ffceff7062b6962fa86a339a42fb82dcc
Vadivel Manjanaicker <vadivelmso@gmail.com>
2016-08-23 21:42:51 +0530

    stringtoint.java

---
+++ stringtoint.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class stringtoint
+{
+  public static void main(String arg[])
+  {
+    Scanner sc=new Scanner(System.in);
+    String str=sc.nextLine();
+    int num=Integer.parseInt(str);
+    System.out.println(num);
+    }
+  }
