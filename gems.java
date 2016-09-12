f25312143849aa8834dce7ad3542a08151244142
Vadivel Manjanaicker <vadivelmso@gmail.com>
2016-09-12 09:06:37 +0530

    gems.java

---
+++ gems.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class gems {
+public static void main(String[] args) {
+int j;
+String s1="hello";
+for( j=0;j<=s1.length();j++)
+{
+char e=s1.charAt(i);
+if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U')){
+{
+String h=s1.substring(0, j);
+String g=s1.substring(i+ j);
+s1=h+"Z"+g;
+}
+}
+System.out.println(s1);
+}
+}
